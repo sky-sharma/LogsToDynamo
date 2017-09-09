855316f51fbc9efaf5765ae12b6c9b1fbe74e97f
skysharma <sky.sharma@gmail.com>
2017-09-08 23:46:06 -0500

    Finally seem to be able to read S3 objects, read dBase and write dBase sequentially. Still some error in calculating TotalNumConnections / Disconnections.

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var params = {
 
 var searchStrings = { Connection: 'Connect Status: SUCCESS', Disconnection: 'Disconnect Status: SUCCESS' };
 var dataSearchPatterns = ['%s %s', 'TRACEID:%s', 'PRINCIPALID:%s', 'IpAddress: %s ', 'SourcePort: %s'];
-var Connections = []; // Clear array of Connections collected from last file
+var ConnectionsThisFile = []; // Clear array of Connections collected from last file
+var logFiles = [];
+var logFileNum = 0;
 
 // var dataArray = [];
 // var AllConnections = [];
@@ -25,85 +27,116 @@ s3.listObjects(params, function(err, data)
   if (err) console.log(err, err.stack); //an error occurred
   else
   {
-    var logFiles = data.Contents;
-    logFiles.forEach((logFile) => {
-      var logFileName = logFile.Key;
-      params.Key = logFileName;
-      // Get Log File Name
+    logFiles = data.Contents;
+    readS3AndGetPutConnection(logFiles, logFileNum++);
+}});
 
-      s3.getObject((params), (err, fileContents) =>
+function readS3AndGetPutConnection(logFiles, logFileIndex)
+{
+  // Recursive function to read s3 item, then put connections in dBase
+  // This involves reading existing numbers of connections / disconnections
+  // from the dBase and adding to the tallies.
+
+  console.log('logFileIndex: ', logFileIndex);
+
+  if (logFileIndex >= logFiles.length) return; // All done
+
+  var logFileName = logFiles[logFileIndex].Key;
+  params.Key = logFileName;
+  // Get Log File Name
+
+  s3.getObject((params), (err, fileContents) =>
+  {
+    // Read contents of fileContents
+    if (err) throw err;
+    var logContents = fileContents.Body;
+
+    ConnectionsThisFile = utils.parseLog(logContents, searchStrings, dataSearchPatterns);
+    getAndPutConnection(ConnectionsThisFile, 0);
+  });
+};
+
+// Put IpAddresses of Connections in Connections table.
+function getAndPutConnection(Connections, ConnectionNum)
+{
+  // Recursive function to make sure dBase read and write happen sequentially.
+  // We read a record from the dBase, get the Number of Connections and Number of Disconnections
+  // from the record and add to those.
+
+console.log(Connections.length);
+
+  if (ConnectionNum >= Connections.length)
+  {
+    readS3AndGetPutConnection(logFiles, logFileNum++);
+    return; // All done.
+  }
+
+  Connection = Connections[ConnectionNum];
+  var PrincipalID = Connection[2];
+  var IpAddress = Connection[3];
+  var Status = Connection[5];
+  var LastConnDisconn = Connection[0][0] + ' ' + Connection[0][1];
+  var TotalNumConnections = Connection[6];
+  var TotalNumDisconnections = Connection[7];
+
+  var dBasePutParams =
+  {
+    TableName: 'Indie_Connections',
+    Item:
+    {
+      'PrincipalID': PrincipalID,
+      'Last_IpAddress': IpAddress,
+      'Current_Status': Status, //Connected or Disconnected
+      'LastConnDisconn_Time': LastConnDisconn, // Concatenating Date and Time
+      'TotalNumConnections': TotalNumConnections,
+      'TotalNumDisconnections': TotalNumDisconnections
+    }
+  }
+
+  var dBaseGetParams =
+  {
+    TableName: 'Indie_Connections',
+    Key:
       {
-        // Read contents of fileContents
-        if (err) throw err;
-        var logContents = fileContents.Body;
+        'PrincipalID': PrincipalID
+      }
+  }
 
-        Connections = utils.parseLog(logContents, searchStrings, dataSearchPatterns);
-        console.log(Connections);
-
-        // Put IpAddresses of Connections in Connections table.
-        Connections.forEach((Connection) => {
+  docClient.get(dBaseGetParams, (err, readRecord) =>
+  {
+    if (err) console.log(err);
+    else if ((Object.keys(readRecord).length === 0) && (readRecord.constructor === Object))
+    // The particular record is not found in the dBase, so enter it
+    {
+      docClient.put(dBasePutParams, (err, data) =>
+      {
+        if (err) console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
+        else
         {
-          var PrincipalID = Connection[2];
-          var IpAddress = Connection[3];
-          var Status = Connection[5];
-          var LastConnDisconn = Connection[0][0] + ' ' + Connection[0][1];
-          var TotalNumConnections = Connection[6];
-          var TotalNumDisconnections = Connection[7];
-
-          var dBasePutParams =
-          {
-          TableName: 'Indie_Connections',
-          Item:
-            {
-              'PrincipalID': PrincipalID,
-              'Last_IpAddress': IpAddress,
-              'Current_Status': Status, //Connected or Disconnected
-              'LastConnDisconn_Time': LastConnDisconn, // Concatenating Date and Time
-              'TotalNumConnections': TotalNumConnections,
-              'TotalNumDisconnections': TotalNumDisconnections
-            }
-          }
-
-          var dBaseGetParams =
-          {
-            TableName: 'Indie_Connections',
-            Key:
-              {
-                'PrincipalID': PrincipalID
-              }
-          }
-
-          docClient.get(dBaseGetParams, (err, readRecord) =>
-          {
-            if (err) console.log(err);
-            else if ((Object.keys(readRecord).length === 0) && (readRecord.constructor === Object))
-            // The particular record is not found in the dBase, so enter it
-            {
-              docClient.put(dBasePutParams, (err, data) =>
-              {
-                if (err) console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
-              });
-            }
-
-            else
-            {
-              // If Status is Connected then take TotalNumConnections for this PrincipalID in dBase,
-              // add new TotalNumConnections and write this new val to dBase.
-
-              // If Status is Disonnected then take TotalNumDisconnections for this PrincipalID in dBase,
-              // add new TotalNumDisconnections and write this new val to dBase.
-
-              dBasePutParams.Item.TotalNumConnections += readRecord.Item.TotalNumConnections;
-              dBasePutParams.Item.TotalNumDisconnections += readRecord.Item.TotalNumDisconnections;
-
-              docClient.put(dBasePutParams, (err, data) =>
-              {
-                if (err) console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
-              });
-            }
-          });
+          console.log('TotalNumDisconnections: ', TotalNumDisconnections);
+          getAndPutConnection(Connections, ConnectionNum + 1); // This function calls itself recursively
         }
       });
-    });
-  })
-}});
+    }
+
+    else
+    {
+      // If Status is Connected then take TotalNumConnections for this PrincipalID in dBase,
+      // add new TotalNumConnections and write this new val to dBase.
+
+      // If Status is Disonnected then take TotalNumDisconnections for this PrincipalID in dBase,
+      // add new TotalNumDisconnections and write this new val to dBase.
+
+      dBasePutParams.Item.TotalNumConnections += readRecord.Item.TotalNumConnections;
+      dBasePutParams.Item.TotalNumDisconnections += readRecord.Item.TotalNumDisconnections;
+      docClient.put(dBasePutParams, (err, data) =>
+      {
+        if (err) console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
+        else
+        {
+          getAndPutConnection(Connections, ConnectionNum + 1); // This function calls itself recursively
+        }
+      });
+    }
+  });
+}
